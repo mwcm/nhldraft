abd6d976ed007d278eb156b5f6411f18f1788df9
mwcm <mwcmitchell@gmail.com>
2018-08-26 17:09:29 -0400

    basic query

--- queries/basic.sql
+++ queries/basic.sql
@@ -1,4 +1,4 @@
-SELECT fname, lname, average_pick, o_rank, season_stats, projected_stats FROM nhl.players
+SELECT pos, fname, lname, average_pick, percent_drafted, o_rank, psr_rank, season_stats, projected_stats FROM nhl.players
 where average_round != '-'
 and average_pick != '-'
 ORDER BY Cast(average_pick as float) ASC
